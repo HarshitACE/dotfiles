091cf7e39130abd12eac51a97c61cdd222232064
Harshit Gautam <hgautam@mercedesamgf1.com>
2025-02-11 09:42:48 +0000

    feat: CPP debugger

--- plugin/lsp.lua
+++ plugin/lsp.lua
@@ -74,7 +74,7 @@ require("mason-lspconfig").setup_handlers {
                         library = {
                             vim.env.VIMRUNTIME
                             -- Depending on the usage, you might want to add additional paths here.
-                            -- "${3rd}/luv/library"
+                            -- "${3rd}/luv/libray"
                             -- "${3rd}/busted/library",
                         }
                         -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
