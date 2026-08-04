039df4afe6b1dc5205cb46e9bafb4b4dca1d98d5
Harshit Gautam <batman@Harshits-MacBook-Pro.local>
2026-08-04 11:41:24 +0100

    feat: add typst

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -635,6 +635,12 @@ return {
   { 'mbbill/undotree' },
   { 'tpope/vim-fugitive' },
   {
+    'chomosuke/typst-preview.nvim',
+    lazy = false, -- or ft = 'typst'
+    version = '1.*',
+    opts = {}, -- lazy.nvim will implicitly calls `setup {}`
+  },
+  {
     "ThePrimeagen/refactoring.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
